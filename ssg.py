724b0f2a6da18ad498749e09a173069ed9f56094
Marc-Oliver Gosselin <mogosselin@mogosselin.com>
2022-02-07 21:42:07 -0500

    Fix directory paths

--- ssg.py
+++ ssg.py
@@ -1,12 +1,10 @@
 import typer
 from ssg.site import Site
 
-def main(source="content", dest="dist"):
-    config = {
-        "dest": dest,
-        "source": source
-    }
 
-    site = Site(**config).build()
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest}
+    Site(**config).build()
+
 
 typer.run(main)
